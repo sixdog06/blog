f5b8d4892a303c39427a3d07e1fc2c7434345fbc
huanruiz <huanruiz@foxmail.com>
2021-07-25 22:53:32 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ hugo # 生成public文件夹
 cd public
 echo "punchcode.top" > CNAME
 git init
-git remote set-url origin https://github.com/huanruiz/huanruiz.github.io.git
-git remote add origin https://github.com/huanruiz/huanruiz.github.io.git
+git remote set-url origin git@github.com:huanruiz/huanruiz.github.io.git
+git remote add origin git@github.com:huanruiz/huanruiz.github.io.git
 git add .
 git commit -m 'init'
 git push -f --set-upstream origin master
