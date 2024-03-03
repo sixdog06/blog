26e342cc3591e1a682f189c941188d43d163a9b2
huanruiz <huanruiz@foxmail.com>
2024-03-03 23:40:51 +0800

    script update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 hugo # 生成public文件夹
 cd public
 git init
-git remote set-url origin git@github.com:xqt01/xqt01.github.io.git
-git remote add origin git@github.com:xqt01/xqt01.github.io.git
+git remote set-url origin https://github.com/xqt01/xqt01.github.io.git
+git remote add origin https://github.com/xqt01/xqt01.github.io.git
 git add .
 git commit -m 'init'
 git push -f --set-upstream origin master
